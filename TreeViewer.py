2550e41e15113df397ee3880185273507021fbea
leandrofavila <leandrofavila@gmail.com>
2024-02-20 16:27:17 -0300

    precisa virar o sentido

--- TreeViewer.py
+++ TreeViewer.py
@@ -1,31 +1,52 @@
-from conecta_db import Conn_DB
-from flask import Flask, render_template, request, redirect, session, flash, url_for
+from flask import Flask, render_template
+from conecta_db import DB
+import pandas as pd
 
-
-class Carregamento:
-    def __init__(self, carregamento):
-        self.carregamento = carregamento
-
-
-lista_ordens = []
 app = Flask(__name__)
-app.secret_key = 'kbca'
+carregamento = '433000'
+carre = DB()
+car = carre.car(carregamento)
+
+df = pd.DataFrame(car)
+#print(df[df['DESC_TECNICA'].str.endswith('EXP')]['NUM_ORDEM'].tolist())
+
+
+
+def generate_tree(df, carregamento):
+    tree_html = f'<div class="tree"><ul><li><a href="#">{carregamento}</a><ul>'
+    for index, row in df.iterrows():
+        if row["DESC_TECNICA"].endswith('EXP'):
+            tree_html += f'<li><a href="#">{row["NUM_ORDEM"]}</a>'
+            next_level_html = generate_next_level_tree(row["NUM_ORDEM"])
+            if next_level_html:
+                tree_html += '<ul>'
+                tree_html += next_level_html
+                tree_html += '</ul>'
+            tree_html += '</li>'
+    tree_html += '</ul></div>'
+    return tree_html
+
+
+def generate_next_level_tree(num_ordem):
+    df_lv2 = pd.DataFrame(carre.filhos(carregamento, num_ordem))
+    if df_lv2.empty:
+        return ""
+    next_level_html = "<ul>"
+    for _, row in df_lv2.iterrows():
+        next_level_html += f'<li><a href="#">{row["NUM_ORDEM"]}</a>'
+        next_level_html += generate_next_level_tree(row["NUM_ORDEM"])
+        next_level_html += "</li>"
+    next_level_html += "</ul>"
+    return next_level_html
+
+
 
 
 @app.route('/')
-def index():
-    return render_template('template.html', titulo='Ordens', itens=lista_ordens)
-
-
-@app.route('/criar', methods=['POST', ])
 def criar():
-    carrega = request.form['ordem']
-    car = Conn_DB(carrega)
-    df = car.focco()
-    nv_1 = df.loc[df['DESC_TECNICA'].str.contains("EXP"), 'NUM_ORDEM']
-    print(nv_1)
-    return redirect(url_for('index'))
+    tree_html = generate_tree(df, carregamento)
+    return render_template('template.html', tree_html=tree_html)
 
 
 if __name__ == '__main__':
-    app.run(host='10.40.3.48', port=8000, debug=True)
+    app.run(debug=True)
--- conecta_db.py
+++ conecta_db.py
@@ -2,48 +2,68 @@ import pandas as pd
 import cx_Oracle
 
 
-class Conn_DB:
-    def __init__(self, carregamento):
-        self.carregamento = carregamento
+class DB:
+    def __init__(self):
+        self.db_connection = None
 
     @staticmethod
-    def connection():
+    def get_connection():
         dsn = cx_Oracle.makedsn("10.40.3.10", 1521, service_name="f3ipro")
         connection = cx_Oracle.connect(user=r"focco_consulta", password=r'consulta3i08', dsn=dsn, encoding="UTF-8")
         cur = connection.cursor()
         return cur
 
-    def focco(self):
-        carr = self.carregamento
-        print(carr)
-        cur = self.connection()
+    def car(self, carregamento):
+        cur = self.get_connection()
         cur.execute(
             r"SELECT DISTINCT TPL.COD_ITEM,  "
             r"TOR.NUM_ORDEM, "
             r"TOR.QTDE,  "
-            r"TIT.DESC_TECNICA  "
+            r"TOR.tipo_ordem, "
+            r"TIT.DESC_TECNICA, "
+            r"TFUN.NOME AS PLANEJADOR, "
+            r"PDV.NUM_ITEM "
             r"FROM FOCCO3I.TITENS_PLANEJAMENTO TPL "
-            r"INNER JOIN FOCCO3I.TITENS_EMPR EMP      ON TPL.ITEMPR_ID = EMP.ID "
-            r"INNER JOIN FOCCO3I.TITENS TIT           ON EMP.ITEM_ID = TIT.ID  "
-            r"INNER JOIN FOCCO3I.TORDENS TOR          ON TPL.ID = TOR.ITPL_ID "
-            r"INNER JOIN FOCCO3I.TDEMANDAS TDE        ON TOR.ID = TDE.ORDEM_ID "
-            r"INNER JOIN FOCCO3I.TORDENS_ROT ROT      ON TOR.ID = ROT.ORDEM_ID "
-            r"INNER JOIN FOCCO3I.TORD_ROT_FAB_MAQ FAB ON ROT.ID = FAB.TORDEN_ROT_ID "
-            r"INNER JOIN FOCCO3I.TMAQUINAS MAQ        ON FAB.MAQUINA_ID = MAQ.ID "
-            r"INNER JOIN FOCCO3I.TITENS_PLAN_FUNC PLA ON TPL.ID = PLA.ITPL_ID "
-            r"INNER JOIN FOCCO3I.TFUNCIONARIOS TFUN   ON PLA.FUNC_ID = TFUN.ID "
-            r"INNER JOIN FOCCO3I.TSRENG_ORDENS_VINC_CAR VINC ON TOR.ID = VINC.ORDEM_ID "
-            r"INNER JOIN FOCCO3I.TSRENGENHARIA_CARREGAMENTOS CAR  ON VINC.CARERGAM_ID = CAR.ID "
+            r"INNER JOIN FOCCO3I.TITENS_EMPR EMP          ON TPL.ITEMPR_ID = EMP.ID "
+            r"INNER JOIN FOCCO3I.TITENS TIT               ON EMP.ITEM_ID = TIT.ID  "
+            r"INNER JOIN FOCCO3I.TORDENS TOR              ON TPL.ID = TOR.ITPL_ID "
+            r"INNER JOIN FOCCO3I.TDEMANDAS TDE            ON TOR.ID = TDE.ORDEM_ID "
+            r"INNER JOIN FOCCO3I.TORDENS_ROT ROT          ON TOR.ID = ROT.ORDEM_ID "
+            r"INNER JOIN FOCCO3I.TORD_ROT_FAB_MAQ FAB     ON ROT.ID = FAB.TORDEN_ROT_ID "
+            r"INNER JOIN FOCCO3I.TMAQUINAS MAQ            ON FAB.MAQUINA_ID = MAQ.ID "
+            r"INNER JOIN FOCCO3I.TITENS_PLAN_FUNC PLA     ON TPL.ID = PLA.ITPL_ID "
+            r"INNER JOIN FOCCO3I.TFUNCIONARIOS TFUN       ON PLA.FUNC_ID = TFUN.ID "
+            r"INNER JOIN FOCCO3I.TORDENS_VINC_ITPDV IT    ON IT.ORDEM_ID = TOR.ID "
+            r"INNER JOIN FOCCO3I.TITENS_PDV PDV           ON PDV.ID = IT.ITPDV_ID "
             r"WHERE TOR.ID IN( "
             r"                SELECT TOR.ID "
             r"                FROM FOCCO3I.TORDENS TOR "
             r"                INNER JOIN FOCCO3I.TSRENG_ORDENS_VINC_CAR VINC      ON TOR.ID = VINC.ORDEM_ID "
             r"                INNER JOIN FOCCO3I.TSRENGENHARIA_CARREGAMENTOS CAR  ON VINC.CARERGAM_ID = CAR.ID "
-            r"                AND CAR.SITUACAO = 'A' "
-            r"                AND CAR.CARREGAMENTO in (" + carr + ") "
+            r"                WHERE CAR.CARREGAMENTO IN (" + str(carregamento) + ") "
             r"                ) "
+            r"GROUP BY TPL.COD_ITEM, TOR.NUM_ORDEM, TOR.QTDE, TIT.DESC_TECNICA, TFUN.NOME, TOR.tipo_ordem, PDV.NUM_ITEM "
         )
         df = cur.fetchall()
-        df = pd.DataFrame(df, columns=['COD_ITEM', 'NUM_ORDEM', 'QTDE', 'DESC_TECNICA'])
-        print(df.to_string())
+        df = pd.DataFrame(df, columns=['COD_ITEM', 'NUM_ORDEM', 'QTDE', 'TIPO_ORDEM', 'DESC_TECNICA', 'PLANEJADOR',
+                                       'NUM_ITEM'])
+        df['QTDE'] = df['QTDE'].astype(int)
+        return df
+
+
+    def filhos(self, carregamento, ordem):
+        cur = self.get_connection()
+        cur.execute(
+            r"SELECT DISTINCT OPFI.NUM_ORDEM, TPL.COD_ITEM, OPPAI.DT_EMISSAO "
+            r"FROM FOCCO3I.TORDENS OPPAI "
+            r"INNER JOIN FOCCO3I.TDEMANDAS TDE                    ON TDE.ORDEM_ID = OPPAI.ID "
+            r"INNER JOIN FOCCO3I.TORDENS OPFI                     ON OPFI.ITPL_ID = TDE.ITPL_ID "
+            r"INNER JOIN FOCCO3I.TSRENG_ORDENS_VINC_CAR VINC      ON VINC.ORDEM_ID = OPFI.ID "
+            r"INNER JOIN FOCCO3I.TSRENGENHARIA_CARREGAMENTOS CAR  ON CAR.ID = VINC.CARERGAM_ID "
+            r"INNER JOIN FOCCO3I.TITENS_PLANEJAMENTO TPL          ON TPL.ID = OPFI.ITPL_ID "
+            r"WHERE OPPAI.NUM_ORDEM IN ("+str(ordem)+") "
+            r"AND CAR.CARREGAMENTO = "+str(carregamento)+" "
+        )
+        df = pd.DataFrame(cur.fetchall(), columns=["NUM_ORDEM", "COD_ITEM", "DT_EMISSAO"])
+        cur.close()
         return df
